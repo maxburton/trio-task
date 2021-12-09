ea4d1cb79d2e532a17287f1b872c89e492e2ebd4
Max Burton <35375562+maxburton@users.noreply.github.com>
2021-12-09 15:28:33 +0000

    added docker_run.sh

---
+++ docker_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker network create trionet
+cd nginx
+docker run -d -p 80:80 --name nginx --network trionet --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx
+cd ../flask-app
+docker build -t flask-app .
+docker run -d -p 5000:5000 --name flask-app --network trionet flask-app
+cd ../db
+docker build -t mysql .
+docker run -d -p --name mysql --network trionet mysql
